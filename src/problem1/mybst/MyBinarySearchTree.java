595afbbbe15560d815d510199311ca28c6488c50
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-27 14:48:56 +0530

    implementing Interface in MyBinarySearchTree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,6 +6,10 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
-public class MyBinarySearchTree<E> {
+public class MyBinarySearchTree<E> implements TreeAdt<E> {
 
+    @Override
+    public boolean add(E data) {
+        return false;
+    }
 }
