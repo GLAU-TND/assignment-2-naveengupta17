d6a5ee8ca9e9267696fced20b3aa06feba03c6ef
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-27 15:50:51 +0530

    commenting conclusion of problem2   case2  false

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -24,6 +24,7 @@ public class MyMain {
         System.out.println("Pre order traversing");
         myBinarySearchTree.traversePreOrder(myBinarySearchTree.getRoot());
         // here first element in pre order same as last element in post order//
+        // here middle Node/element in preOrder and PostOrder traversing not same so false //
 
     }
 
