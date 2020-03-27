af0731ca0f6a2b0b7d15b7e4db6d4d0e107039b6
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-27 15:18:14 +0530

    create addRecursive method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,11 +5,38 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements TreeAdt<E> {
+    int counter = 0;
+    private TreeNode<E> root;
+    private int noOfNodes = 0;
+
+    private TreeNode<E> addRecursive(TreeNode<E> currentNode, E data) {
+        if (currentNode == null) {
+            return new TreeNode<E>(data);
+        }
+        if (((Integer) data) < (Integer) currentNode.getData()) {
+            currentNode.setLeftChild(addRecursive(currentNode.getLeftChild(), data));
+
+        } else if (((Integer) data) >= (Integer) currentNode.getData()) {
+            currentNode.setRightChild(addRecursive(currentNode.getRightChild(), data));
+        }
+        return currentNode;
+
+    }
+
 
     @Override
     public boolean add(E data) {
         return false;
+
     }
+
+
+
+
 }
+
