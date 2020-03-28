112d159126a3a4fc5d48e702d83a3d448539744e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-28 16:15:01 +0530

    created iterface QADT

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,5 +6,34 @@
  */
 package problem5.student;
 // to store student information and properties
-public class Student {
+public class Student implements Comparable<Student> {
+    private String name = null;
+    private int rollNo = 0;
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+
+    @Override
+    public int compareTo(Student student) {
+        return this.getRollNo() student.getRollNo();
+    }
 }
