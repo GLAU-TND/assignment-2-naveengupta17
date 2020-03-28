55476183e05925699ee7e711ee77c57bec1c650e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-28 16:43:56 +0530

    overrided method equals and hashcode

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -1,5 +1,7 @@
 package problem3.student;
 
+import java.util.Objects;
+
 public class Student {
     private int rollNumber;
     private String name;
@@ -20,6 +22,24 @@ public class Student {
     @Override
     public String toString() {
         return rollNumber + ", " + name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean flag = false;
+        Student student = (Student) o;
+        if (rollNumber > student.rollNumber) {
+            return true;
+        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        return flag;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rollNumber);
     }
 
     public String getName() {
