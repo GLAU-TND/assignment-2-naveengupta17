742704e5f38fb16cb8c0661630583b8a4fe67828
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-28 16:33:54 +0530

    created getter and setter

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -9,5 +9,19 @@ public class Student {
         this.name = name;
     }
 
+    public int getRollNumber() {
+        return rollNumber;
+    }
 
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
