61fdd85d294aec01aec5b277c80439a7680cb6df
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-28 17:29:31 +0530

    implement ADT i MyQueue

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,31 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
-public class MyQueue {
+import problem4.adt.ADT;
+
+public class MyQueue<E> implements ADT<E> {
+    @Override
+    public void enqueue(E data) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
 }
