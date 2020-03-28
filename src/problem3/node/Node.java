c0e31d54df935ed35fc8e904f8f260c8d28fe8d1
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-03-28 16:26:09 +0530

    created constructor of Node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,6 +6,17 @@
  */
 package problem3.node;
 
-public class Node {
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
 
 }
